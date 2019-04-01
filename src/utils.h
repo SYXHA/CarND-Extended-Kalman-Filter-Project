65a1d961ff40622174bb07af97b099dec164aef0
Yuanxun Shao <yuanxus@g.clemson.edu>
2019-03-31 20:10:18 -0400

    first update contents

---
+++ src/utils.h
@@ -0,0 +1,20 @@
+#ifndef Utils_H_
+#define Utils_H_
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+string hasData(string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_first_of("]");
+  if (found_null != string::npos) {
+    return "";
+  }
+  else if (b1 != string::npos && b2 != string::npos) {
+    return s.substr(b1, b2 - b1 + 1);
+  }
+  return "";
+}
+
+#endif
